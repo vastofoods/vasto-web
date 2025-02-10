b2077242efbb55b7fd5f8a224f8d1b4eacb5f748
Luca Pattin <lupattin@gmail.com>
2025-02-10 19:59:14 -0300

    add fonts add color palette

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Initial from "@/components/inital/inital";
+import WhyVasto from "@/components/whyvasto/whyvasto";
 export default function Home() {
   return (
     <div >
       <Initial />
+      <WhyVasto />
     </div>
   );
 }
---
+++ src/components/whyvasto/whyvasto.jsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.css'
+import Image from 'next/image'
+export default function WhyVasto () {
+    
+
+    return (
+      <div className={styles.container} >
+        <div>
+            <div>
+                <h5>Why <Image ></Image> ?</h5>
+            </div>
+            <div>
+                <div> <p>B2B platform supplying F&V to regional supermarkets</p> </div>
+                <div> <p>For Producers</p> </div>
+                <div> <p>For Regional Supermarket</p> </div>
+            </div>
+        </div>
+      </div>
+    ); 
+  }
