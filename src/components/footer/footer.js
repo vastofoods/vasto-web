e397cec5936b9b1a3ea41c6c64920fabb7d8e8fb
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-11 00:50:00 -0300

    footer styles part 1

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,9 @@
 import styles from './styles.module.css'
+import Image from 'next/image';
 export default function Footer() {
     return (
       <div className={styles.container}>
-        Footer
+        <Image src={'/carouselfootervasto.png'} alt='carousel vasto quality' fill></Image>
       </div>
     );
   }
