73597fb3335dd7c34efcbaf386f72c9afe8fd7f4
Luca Pattin <lupattin@gmail.com>
2025-02-10 21:27:25 -0300

    add whyvasto and imges

--- src/components/whyvasto/whyvasto.jsx
+++ src/components/whyvasto/whyvasto.jsx
@@ -5,14 +5,17 @@ export default function WhyVasto () {
 
     return (
       <div className={styles.container} >
-        <div>
-            <div>
-                <h5>Why <Image src={"/vasto.svg"} width={50} height={50} alt=''></Image> ?</h5>
+        <div className={styles.textContainer}>
+            <div className={styles.titleContainer}>
+                <h5 className={styles.title}>
+                  Why 
+                  <Image src={"./img/vastoR.svg"} width={250} height={150} alt=''></Image>
+                </h5>
             </div>
             <div>
-                <div> <p>B2B platform supplying F&V to regional supermarkets</p> </div>
-                <div> <p>For Producers</p> </div>
-                <div> <p>For Regional Supermarket</p> </div>
+                <div className={styles.animatedBox}> <p>B2B platform supplying F&V to regional supermarkets</p> </div>
+                <div className={styles.animatedBox}>  <p>For Producers</p> </div>
+                <div className={styles.animatedBox}> <p>For Regional Supermarket</p> </div>
             </div>
         </div>
       </div>
