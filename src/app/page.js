1454f89907256a201a55bcb27268efb68b2fc8a2
Luca Pattin <lupattin@gmail.com>
2025-02-11 00:01:56 -0300

    add ours

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Initial from "@/components/inital/inital";
 import WhyVasto from "@/components/whyvasto/whyvasto";
+import Ours from "@/components/ours/ours";
 export default function Home() {
   return (
     <div >
       <Initial />
       <WhyVasto />
+      <Ours />
     </div>
   );
 }
