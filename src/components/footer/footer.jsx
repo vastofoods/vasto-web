3123f1bc37e88c3083547e06b79823f72eed6982
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-11 21:36:07 -0300

    fixing footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Footer() {
     return (
       <div className={styles.container}>
-        <Image src={'/img/carouselfootervasto.png'} alt='carousel vasto quality' fill></Image>
+        ver
       </div>
     );
   }
