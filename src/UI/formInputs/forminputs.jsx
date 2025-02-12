49a3b678176c23fff48bc00257634a40b2260f5d
Luca Pattin <lupattin@gmail.com>
2025-02-11 21:29:30 -0300

    finishing contactForm

--- src/UI/formInputs/forminputs.jsx
+++ src/UI/formInputs/forminputs.jsx
@@ -1,18 +1,30 @@
 import styles from './styles.module.css'
-export function FormInputs({text}) {
-    return (
-      <div className={styles.container}>
-        <label className={styles.label} htmlFor="">{text}</label>
-        <input className={styles.input} type="text" />
-      </div>
-    );
-  }
+export function FormInputs({ text, placeholder, type }) {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label} htmlFor="">{text}</label>
 
-export function FormTextArea() {
+      {type === "dropdown" ? (
+        <select className={styles.input} placeholder={placeholder}>
+          <option value="" disabled hidden className={styles.placeholderOption}>
+            Select an option
+          </option>
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      ) : (
+        <input className={styles.input} type={type} placeholder={placeholder} />
+      )}
+    </div>
+  );
+}
+
+export function FormTextArea({text, placeholder}) {
     return (
-      <div className={styles.container}>
-        <label htmlFor=""></label>
-        <textarea type="text" />
+      <div className={styles.bigContainer}>
+        <label className={styles.bigLabel} htmlFor="">{text} </label>
+        <textarea className={styles.bigInput} type="text" placeholder={placeholder}/>
       </div>
     );
   }
--- src/components/contactform/contactform.jsx
+++ src/components/contactform/contactform.jsx
@@ -10,13 +10,16 @@ export default function ContactForm() {
         </div>
         <div>
           <div className={styles.containerInputsForm}>
-            <FormInputs text={'First name'} />
-            <FormInputs text={'Last name'} />
-            <FormInputs text={"Work e-mail address"}/>
-            <FormInputs text={"Company"}/>
-            <FormInputs text={"Institution Type"}/>
-            <FormInputs text={"Phone number"}/>
-            <FormTextArea />
+            <FormInputs text={'First name'} placeholder={"Susan"} type={"text"}/>
+            <FormInputs text={'Last name'} placeholder={"Parker"} type={"text"}/>
+            <FormInputs text={"Work e-mail address"} placeholder={"susanparker@company.com"} type={"email"}/>
+            <FormInputs text={"Company"} placeholder={"-"} type={"text"}/>
+            <FormInputs text={"Institution Type"} placeholder={"Select an option"} type={"dropdown"}/>
+            <FormInputs text={"Phone number"} placeholder={"1122334455"}/>
+            <FormTextArea text={"Your message"} />
+          </div>
+          <div className={styles.containerButton}>
+            <button className={styles.button}>Send</button>
           </div>
         </div>
       </div>
