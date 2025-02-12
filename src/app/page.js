3fbafff56695bbb5ec35323a34d7823d8f0ec9a2
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-12 14:00:59 -0300

    fixing footer errors but making new ones - 3 components created

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 import Initial from "@/components/inital/inital";
+import Welcomeinitial from "@/components/welcomeinitial/welcomeinitial";
+import Thesolution from "@/components/thesolution/thesolution";
+import Howitworks from "@/components/howitworks/howitworks";
 import WhyVasto from "@/components/whyvasto/whyvasto";
 import Ours from "@/components/ours/ours";
 import ContactForm from "@/components/contactform/contactform";
@@ -6,6 +9,9 @@ export default function Home() {
   return (
     <div >
       <Initial />
+      <Welcomeinitial/>
+      <Thesolution/>
+      <Howitworks/>
       <WhyVasto />
       <Ours />
       <ContactForm />
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,26 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import Image from 'next/image';
+import {InstagramLogo, LinkedinLogo} from 'phosphor-react';
 
 export default function Footer() {
   const text = "* VASTO QUALITY ";
   const repetitions = 7;
   return (
-  <p className='containerText'>{text.repeat(repetitions)}
-  </p>
+    <div className={styles.container}>
+      <div className={styles.containerText}>
+      {Array(repetitions).fill(text).map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+    </div>
+    <div>
+      <Image src={"/img/footerLogo.png"} width={190} height={92} alt='Isologo Vasto Foods'></Image>
+      <p className={styles.containerBrandsMotto}>Cultivating Partnership, Harvesting Prosperity</p>
+      <div className={styles.containerIcons}>
+      <LinkedinLogo size={32} />
+      <InstagramLogo size={32} />
+      </div>
+      <p>copyright</p>
+    </div>
+  </div>
   )
 }
