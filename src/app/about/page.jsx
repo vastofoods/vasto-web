bccc857077c2b331bdec20784fcc1367daf2c658
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-13 13:34:25 -0300

    completed initial about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.css'
+import Image from 'next/image';
 export default function About() {
-    
-
     return (
       <div className={styles.container} >
-        About Page
+        <Image className={styles.containerImg} src={'/img/footerLogo.png'} width={800}  height={800} alt='logo de vasto foods'></Image>
+      <div className={styles.containerParagraph}>
+        <p className={styles.containerParagraphText}>Optimizing the fresh produce supply chain from sourcing to sales</p>
+      </div>
       </div>
     );
   }
