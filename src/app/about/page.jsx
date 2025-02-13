6a95285ee941af6505961516afc0740c2499acda
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-13 15:55:35 -0300

    some changes in about and cards

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,13 @@
 import styles from './styles.module.css'
 import Image from 'next/image';
+
 export default function About() {
-    return (
-      <div className={styles.container} >
-        <Image className={styles.containerImg} src={'/img/footerLogo.png'} width={800}  height={800} alt='logo de vasto foods'></Image>
+  return (
+    <div className={styles.container} >
+      <Image className={styles.containerImg} src={'/img/footerLogo.png'} width={800} height={800} alt='logo de vasto foods'></Image>
       <div className={styles.containerParagraph}>
         <p className={styles.containerParagraphText}>Optimizing the fresh produce supply chain from sourcing to sales</p>
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
