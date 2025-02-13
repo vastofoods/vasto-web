3612b36db8e20ac9deefe1601f10f793f3099d06
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-12 22:25:15 -0300

    completed welcome initial

--- src/components/welcomeinitial/welcomeinitial.jsx
+++ src/components/welcomeinitial/welcomeinitial.jsx
@@ -3,12 +3,8 @@ import styles from './styles.module.css'
 export default function Welcomeinitial() {
     return (
         <div className={styles.container}>
-            <p className={styles.containerText}>Regional supermarkets <b className={styles.containerBold}>suffer from 
-significant volatility in quality, supply, and 
-price of produce</b> due to highly marginalized 
-suppliers, less purchasing power, and less 
-technology than their national and 
-international competitors</p>
+            <p className={styles.containerText}>Regional supermarkets <b className={styles.containerBold}>suffer from significant volatility in quality, supply, <br /> and 
+            price of produce</b> due to highly marginalized suppliers, less purchasing power, <br /> and less technology than their national and international competitors</p>
         </div>
     );
 }
