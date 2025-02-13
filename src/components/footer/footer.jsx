9601b014f1216a7efd5ab35fb6f72c4a294d4ef7
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-12 21:45:38 -0300

    completed footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,11 @@
 import styles from './styles.module.css';
 import Image from 'next/image';
-import {InstagramLogo, LinkedinLogo} from '@phosphor-icons/react';
+import Link from "next/link";
+import { InstagramLogo, LinkedinLogo, Phone, MapPinLine} from '@phosphor-icons/react/dist/ssr';
 
 export default function Footer() {
   const text = "* VASTO QUALITY ";
-  const repetitions = 7;
+  const repetitions = 6;
   return (
     <div className={styles.container}>
       <div className={styles.containerText}>
@@ -12,14 +13,18 @@ export default function Footer() {
         <p key={index}>{line}</p>
       ))}
     </div>
-    <div>
+    <div className={styles.containerLogoredes}>
       <Image src={"/img/footerLogo.png"} width={190} height={92} alt='Isologo Vasto Foods'></Image>
       <p className={styles.containerBrandsMotto}>Cultivating Partnership, Harvesting Prosperity</p>
-      <div className={styles.containerIcons}>
-      <LinkedinLogo size={32} />
-      <InstagramLogo size={32} />
+      <div>
+      <div className={styles.contactDetails}><Phone size={32} /><p className={styles.contactText}>+528115167342</p></div>
+      <div className={styles.contactDetails}><MapPinLine size={32} /><p className={styles.contactText}>San Pedro Garza Garcia, NL, 66256</p></div>
       </div>
-      <p>copyright</p>
+      <div className={styles.containerIcons}>
+      <Link href={'.7'}><LinkedinLogo size={32} /></Link>
+      <Link href={'.7'}><InstagramLogo size={32} /></Link>
+      </div>
+      <p className={styles.containerCopy}>@VASTO FOODS 2025 - copyright florpastudio</p>
     </div>
   </div>
   )
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 import Link from "next/link";
 import Image from 'next/image';
 export default function Navbar() {
